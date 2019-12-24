5a7a7082da4b9abf5f2a1a75e98499ef15d5881d
madhanbala1 <madhanbalaji@codingmart.com>
2019-12-24 17:39:40 +0530

    third commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "git@github.com:madhanbala1/book-store-app.git"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/#{fetch :application}"
-set :tmp_dir, "/home/deploy/tmp"
+set :tmp_dir, "/var/www/tmp"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
